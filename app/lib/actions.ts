21f69740bac9b0584842b103bcc46ae3b35a34df
Andres Campuzano Garzon <campuxano88@gmail.com>
2024-03-10 12:41:38 -0500

    Updated invoice creation page and form functionality
    Enhanced the invoice creation page with additional features including a fetching customers method and navigation breadcrumbs. Also updated the invoice creation form, now it makes use of action from the newly added `createInvoice` function. Zod package is also updated.

---
+++ app/lib/actions.ts
@@ -0,0 +1,14 @@
+'use server'
+
+
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    }
+
+    console.log(rawFormData)
+
+    // const rawFormData = Object.fromEntries(formData.entries())
+}
